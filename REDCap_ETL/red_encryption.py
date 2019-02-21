bd13a45f1938e91c4f96c02acb04e2eda92bd2a7
Mkang1204 <mengjiakang2017@u.northwestern.edu>
2019-02-21 16:49:59 -0600

    network vis python

--- REDCap_ETL/red_encryption.py
+++ REDCap_ETL/red_encryption.py
@@ -4,12 +4,12 @@ import datetime
 import hashlib
 import random
 import redcap
+# import pycap
 
+from redcap import Project
+salt = "abcdefghijklmn"
 
-from pycap import Project
-salt = "SOME RANDOM STRING"
-
-project = Project(URL, TOKEN)
+project = Project('https://redcap.nubic.northwestern.edu/redcap/api/', '9A8D419989B3518D4291C3F82AEE6A7E')
 
 phi_records = project.export_records(format='df')
 
@@ -24,6 +24,6 @@ def obfuscate_date(date_value):
     return date_value + td
 
 phi_records['record_id'] = phi_records['record_id'].map(obfuscate_id)
-phi_records['sensitive_date'] = phi_records['sensitive_date'].map(obfuscate_date)
+# phi_records['sensitive_date'] = phi_records['sensitive_date'].map(obfuscate_date)
 
-phi_records.to_csv(....)
+phi_records.to_csv('/Users/mkh1805/Box/NU-SCRIPT/REDCap/TestExport/records_all02210105py.csv', header=True,encoding = 'utf-8', sep = ',')
